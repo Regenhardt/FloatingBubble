b927f5ab3ff7ec44b4a74a2af5ec6488b678108e
Miles <NaN>
2017-06-16 14:50:17 +0200

    Eddited the startup file and  added a unhandled exception catching

--- FloatingBubble/App.xaml.cs
+++ FloatingBubble/App.xaml.cs
@@ -12,6 +12,10 @@ namespace FloatingBubble
 	/// Interaction logic for App.xaml
 	/// </summary>
 	public partial class App : Application
-	{
-	}
+    {
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+
+        }
+    }
 }
