fba41a9c3174f0874e27d73cf16b73fe390ff2ff
Miles <NaN>
2017-06-16 16:14:14 +0200

    Added bubbleviewmodel

--- FloatingBubble/Views/Bubble.xaml.cs
+++ FloatingBubble/Views/Bubble.xaml.cs
@@ -1,27 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace FloatingBubble
 {
-	/// <summary>
-	/// Interaction logic for Bubble.xaml
-	/// </summary>
-	public partial class Bubble : Window
+    /// <summary>
+    /// Interaction logic for Bubble.xaml
+    /// </summary>
+    public partial class Bubble : Window
 	{
 		public Bubble()
 		{
 			InitializeComponent();
+
+            this.DataContext = new Viewmodels.BubbleViewmodel();
 		}
 
         /// <summary>
