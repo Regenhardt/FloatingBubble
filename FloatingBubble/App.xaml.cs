076b99822a32f24694a726cbb05b6aee80cb7584
Miles <NaN>
2017-06-16 14:56:53 +0200

    added base logic for the dispatcher

--- FloatingBubble/App.xaml.cs
+++ FloatingBubble/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace FloatingBubble
 {
@@ -13,8 +14,15 @@ namespace FloatingBubble
 	/// </summary>
 	public partial class App : Application
     {
-        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        /// <summary>
+        /// Any exection that isn't caught by the application will be handled here
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Application_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
         {
+            MessageBox.Show("Unexpected error occured.");
+            //TODO: LOG it and think of a good way to handle it
 
         }
     }
