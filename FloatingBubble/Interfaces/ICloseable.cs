379f13654ba162ff1e462becf89232d67cff1baa
Miles <NaN>
2017-06-16 17:00:40 +0200

    Implemented an interface to uphold mvvm lateron (passing views as interface)

---
+++ FloatingBubble/Interfaces/ICloseable.cs
@@ -0,0 +1,15 @@
+﻿namespace FloatingBubble.Interfaces
+{
+    /// <summary>
+    /// Interface with main reason to preserve the MVVM pattern --> buisness logic not knowing about views
+    /// Interface making sure that one can hide, how and close a window
+    /// </summary>
+    interface IHideShowAndCloseable
+    {
+        void Close();
+
+        void Hide();
+
+        void Show();
+    }
+}
