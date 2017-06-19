e6b0c8994485485146676402cab3f10e0697cda4
Miles <NaN>
2017-06-19 14:33:52 +0200

    marked obsolete

--- FloatingBubble/Interfaces/IHideShowAndCloseable.cs
+++ FloatingBubble/Interfaces/IHideShowAndCloseable.cs
@@ -4,6 +4,7 @@
     /// Interface with main reason to preserve the MVVM pattern --> buisness logic not knowing about views
     /// Interface making sure that one can hide, how and close a window
     /// </summary>
+    [System.Obsolete("Using now boolean value to switch")]
     interface IHideShowAndCloseable
     {
         void Close();
