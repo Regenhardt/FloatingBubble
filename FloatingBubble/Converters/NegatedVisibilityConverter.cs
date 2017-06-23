0cac5dd196487e0750ec943b2c98dc13dc870e35
Marlon Regenhardt <marlon.r@hotmail.de>
2017-06-23 14:09:53 +0200

    NegatedVisibilityConverter

---
+++ FloatingBubble/Converters/NegatedVisibilityConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace FloatingBubble.Converters
+{
+	[ValueConversion(typeof(bool), typeof(Visibility))]
+	public class NegatedVisibilityConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value is bool)
+			{
+				return (bool)value ? Visibility.Collapsed : Visibility.Visible;
+			}
+			else
+			{
+				return Binding.DoNothing;
+			}
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value is Visibility)
+			{
+				return (Visibility)value == Visibility.Visible ? false : true;
+			}
+			else return Binding.DoNothing;
+		}
+	}
+}
