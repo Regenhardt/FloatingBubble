7bb08a0ec12c7c2b0e84cd1bb235fd5184800a1e
Marlon Regenhardt <marlon.r@hotmail.de>
2017-06-23 14:04:04 +0200

    BaseNotify Upgrade

--- FloatingBubble/BaseNotify.cs
+++ FloatingBubble/BaseNotify.cs
@@ -1,23 +1,39 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
 
 namespace FloatingBubble
 {
-    /// <summary>
-    /// Base class implementing the INotifyPropertyChanged interface.
-    /// Used for Viewmodels and Models.
-    /// </summary>
-    abstract class BaseNotify : INotifyPropertyChanged
-    {
-        #region [ INotifyPropertyChanged Members ]
+	/// <summary>
+	/// Base class implementing the INotifyPropertyChanged interface.
+	/// Used for Viewmodels and Models.
+	/// </summary>
+	abstract class BaseNotify : INotifyPropertyChanged
+	{
+		#region [ INotifyPropertyChanged Members ]
 
-        public event PropertyChangedEventHandler PropertyChanged;
+		public event PropertyChangedEventHandler PropertyChanged;
 
-        protected void RaisePropertyChanged(string propertyName)
-        {
-            if (PropertyChanged != null)
-                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-        }
+		/// <summary>
+		/// Call with (null) to update all properties.
+		/// </summary>
+		/// <param name="propertyName">Name of the Property to update.</param>
+		protected void RaisePropertyChanged(string propertyName)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
 
-        #endregion
-    }
+		/// <summary>
+		/// Call from a property setter to notify about that property.
+		/// </summary>
+		protected void RaisePropertyChanged()
+		{
+			var methodName = new StackFrame(1).GetMethod().Name;
+			if (!methodName.StartsWith("set_"))
+				throw new Exception("This must be called from a property setter.");
+			RaisePropertyChanged(methodName.Substring("set_".Length));
+		}
+
+		#endregion
+	}
 }
